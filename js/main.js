fc37506595bb8d500a08d4f3afc9d5ec65038098
Atru8729 <a.truskauskas1@gmail.com>
2021-06-02 19:14:10 +0300

    Receptas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('labas');
+/*
+Surasti visus ingredientus ir ju pavadinimus atspausdinti i console
+
+Visu ingredientu pavadinimai turi buti didziosiomis raidemis ir sudeti i array
+
+Isspausdinti gauta array i console
+
+*/
+
+const liDOM = document.querySelectorAll('li');
+const ingredientai = [];
+for (let i = 0; i < liDOM.length; i++) {
+    const ingredientas = liDOM[i].innerText.toUpperCase();
+    ingredientai.push(ingredientas);
+}
+
+console.log(ingredientai);
