1d5db873d148563b38a8bd95414bd0e1ee65d682
Atru8729 <a.truskauskas1@gmail.com>
2021-06-03 20:54:34 +0300

    md

--- js/main.js
+++ js/main.js
@@ -28,18 +28,3 @@ h1DOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
 })
-
-
-/*const logoDOM = document.querySelector('.logo');
-const hamburgerDOM = document.querySelector('.hamburger');
-const navDOM = document.querySelector('nav');
-
-console.log(logoDOM);
-console.log(hamburgerDOM);
-console.log(navDOM);
-
-hamburgerDOM.addEventListener('click', () => {
-    navDOM.classList.toggle('visible');
-    logoDOM.classList.toggle('big');
-})
-*/
