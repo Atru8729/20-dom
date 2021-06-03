40ae39239bc8f7197868f733b27e8f5095964914
Atru8729 <a.truskauskas1@gmail.com>
2021-06-03 20:50:51 +0300

    Zaidynes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-const logoDOM = document.querySelector('.logo');
+let rezultatas = 0;
+
+const minusDOM = document.querySelector('.minus');
+const plusDOM = document.querySelector('.plus');
+const resetDOM = document.querySelector('.reset');
+const numberDOM = document.querySelector('.number');
+const h1DOM = document.querySelector('h1');
+
+minusDOM.addEventListener('click', () => {
+    rezultatas = rezultatas - 1;
+    numberDOM.innerText = rezultatas;
+    h1DOM.innerText = 'Žaidimas progrese';
+})
+
+plusDOM.addEventListener('click', () => {
+    rezultatas = rezultatas + 1;
+    numberDOM.innerText = rezultatas;
+    h1DOM.innerText = 'Žaidimas progrese';
+})
+
+resetDOM.addEventListener('click', () => {
+    rezultatas = 0;
+    numberDOM.innerText = rezultatas;
+    h1DOM.innerText = 'Te prasideda žaidynės! 🥳';
+})
+
+h1DOM.addEventListener('click', () => {
+    rezultatas = 0;
+    numberDOM.innerText = rezultatas;
+})
+
+
+/*const logoDOM = document.querySelector('.logo');
 const hamburgerDOM = document.querySelector('.hamburger');
 const navDOM = document.querySelector('nav');
 
@@ -10,3 +42,4 @@ hamburgerDOM.addEventListener('click', () => {
     navDOM.classList.toggle('visible');
     logoDOM.classList.toggle('big');
 })
+*/
