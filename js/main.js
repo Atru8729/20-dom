03f7aa6a8d2c31de3cbfcb3f2a100d813bf14ceb
Atru8729 <a.truskauskas1@gmail.com>
2021-06-03 19:13:14 +0300

    Hamburger

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const logoDOM = document.querySelector('.logo');
 const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector('nav');
 
 console.log(logoDOM);
 console.log(hamburgerDOM);
+console.log(navDOM);
 
 hamburgerDOM.addEventListener('click', () => {
-    logDOM.classList.toggle('big');
+    navDOM.classList.toggle('visible');
+    logoDOM.classList.toggle('big');
 })
