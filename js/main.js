f420df60622dda4148ed64d816f2002036ae9b42
Atru8729 <a.truskauskas1@gmail.com>
2021-06-04 20:11:11 +0300

    Zinuciu generavimas

--- js/main.js
+++ js/main.js
@@ -2,29 +2,31 @@ let rezultatas = 0;
 
 const minusDOM = document.querySelector('.minus');
 const plusDOM = document.querySelector('.plus');
-const resetDOM = document.querySelector('.reset');
 const numberDOM = document.querySelector('.number');
+const resetDOM = document.querySelector('.reset');
 const h1DOM = document.querySelector('h1');
+const ulDOM = document.querySelector('ul');
+
+const gameInProgressMessage = 'Žaidimas progrese';
+const initialH1Text = h1DOM.innerText;
+
+ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`;
 
 minusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas - 1;
-    numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese';
+    numberDOM.innerText = --rezultatas;
+    h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
 
 plusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas + 1;
-    numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese';
+    numberDOM.innerText = ++rezultatas;
+    h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
 
 resetDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Te prasideda žaidynės! 🥳';
-})
-
-h1DOM.addEventListener('click', () => {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
+    h1DOM.innerText = initialH1Text;
+    ulDOM.innerHTML += `<li>Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
