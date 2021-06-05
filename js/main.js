124ba5aad887e78f6fd9c6504637c6ba1a7b9dae
Atru8729 <a.truskauskas1@gmail.com>
2021-06-05 11:48:39 +0300

    11

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const buttonDOM = document.querySelector('button');
 const inputDOM = document.querySelector('input');
 const spanDOM = document.querySelector('span');
+const inputBgColorDOM = document.querySelector('#bg-color');
+const inputTextColorDOM = document.querySelector('#text-color');
+
+inputDOM.style.color = 'red';
+
+
 
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
     spanDOM.innerText = inputDOM.value;
     inputDOM.value = '';
+    spanDOM.style.backgroundColor = inputBgColorDOM.value;
+    spanDOM.style.color = inputTextColorDOM.value;
+
 })
