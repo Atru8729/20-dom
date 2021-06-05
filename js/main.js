6ee4438389e56ece1538d84e1f3103bb68d3c22c
Atru8729 <a.truskauskas1@gmail.com>
2021-06-05 13:54:34 +0300

    meniu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 const buttonDOM = document.querySelector('button');
-const inputDOM = document.querySelector('input');
-const spanDOM = document.querySelector('span');
-const inputBgColorDOM = document.querySelector('#bg-color');
-const inputTextColorDOM = document.querySelector('#text-color');
-
-inputDOM.style.color = 'red';
-
-
+const orderDOM = document.querySelector('.order');
 
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
-    spanDOM.innerText = inputDOM.value;
-    inputDOM.value = '';
-    spanDOM.style.backgroundColor = inputBgColorDOM.value;
-    spanDOM.style.color = inputTextColorDOM.value;
-
-})
+    orderDOM.innerText = 'Uzsakovas vardu [vardas] [nori/nenori] sriubos, [nori/nenori] pagrindinio patiekalo, [nori/nenori] deserto ir [gerymo pavadinimas] yra pasirinktas gerymas.'
+});
