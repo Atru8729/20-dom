ed5885da108b1b93c85a589b6e5e78b8de7d448b
Atru8729 <a.truskauskas1@gmail.com>
2021-06-15 21:24:57 +0300

    Laikrodis vidurnakti ir torto dalybos uzdaviniai

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-function time(nuo, iki, step) {
-    const pamokuKiekis = Math.floor((iki - nuo) * 60 / step);
-    console.log(pamokuKiekis + 1);
-
-    for (let i = nuo * 60; i <= iki * 60; i += step) {
+function time(nuo, iki, trukme) {
+    for (let i = nuo * 60; i <= iki * 60; i += trukme) {
         const h = Math.floor(i / 60);
-        //const h = (i - m) / 60;
-        const m = i % 60;
-        //const m = i - (h * 60);
-
-        console.log(i, `${h}:${m < 10 ? '0' + m : m}`);
+        const m = i - h * 60;
+        console.log(`${h}:${m < 10 ? '0' + m : m}`);
     }
 }
+
 time(8, 11, 45);
-time(1, 4, 31);
