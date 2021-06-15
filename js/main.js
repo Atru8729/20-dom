73fea138b68e2dd7745589b5c62aa99526a065cb
Atru8729 <a.truskauskas1@gmail.com>
2021-06-15 21:21:30 +0300

    19+20

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-function time(nuo, iki) {
-    for (let h = nuo; h < iki; h++) {
-        for (let m = 0; m < 60; m += 15) {
-            console.log(`${h}:${m < 10 ? '0' + m : m}`);
-        }
+function time(nuo, iki, step) {
+    const pamokuKiekis = Math.floor((iki - nuo) * 60 / step);
+    console.log(pamokuKiekis + 1);
 
+    for (let i = nuo * 60; i <= iki * 60; i += step) {
+        const h = Math.floor(i / 60);
+        //const h = (i - m) / 60;
+        const m = i % 60;
+        //const m = i - (h * 60);
+
+        console.log(i, `${h}:${m < 10 ? '0' + m : m}`);
     }
 }
-time(1, 7);
+time(8, 11, 45);
+time(1, 4, 31);
