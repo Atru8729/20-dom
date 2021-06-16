6d9f533035fdf87ed005dc48057b22de117fc7f9
Atru8729 <a.truskauskas1@gmail.com>
2021-06-16 19:24:23 +0300

    valandos po vidurnakcio ir tortai

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-function time(nuo, iki, trukme) {
-    for (let i = nuo * 60; i <= iki * 60; i += trukme) {
-        const h = Math.floor(i / 60);
-        const m = i - h * 60;
-        console.log(`${h}:${m < 10 ? '0' + m : m}`);
-    }
+function tortoGabalas(tortuKiekis, zmoniuKiekis, tortoSvoris) {
+    const zmoniuPerTorta = Math.ceil(zmoniuKiekis / tortuKiekis);
+    return Math.floor(tortoSvoris / zmoniuPerTorta * 1000);
 }
 
-time(8, 11, 45);
+console.log(tortoGabalas(3, 17, 1)); // 166
